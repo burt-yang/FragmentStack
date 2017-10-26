4cf456252a2216752ecc445e4d2658e24d1c4f64
burt.yang <burt.yang@pwc.com>
2017-10-26 12:26:58 +0800

    finish demo

--- app/src/main/java/com/smart/control/section/FragmentStack.java
+++ app/src/main/java/com/smart/control/section/FragmentStack.java
@@ -1,7 +1,6 @@
 package com.smart.control.section;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
@@ -12,21 +11,20 @@ import java.util.LinkedList;
  */
 public final class FragmentStack {
     private LinkedList<Fragment> mStack = new LinkedList<>();
-    private FragmentActivity mActivity;
     private FragmentManager mFragmentManager;
     private int mContainerId;
+    private TopFragmentChangeListener mTopFragmentChangeListener;
 
-    private FragmentStack(FragmentActivity activity, int containerId) {
-        this.mActivity = activity;
-        mFragmentManager = activity.getSupportFragmentManager();
+    private FragmentStack(FragmentManager fragmentManager, int containerId) {
+        mFragmentManager = fragmentManager;
         this.mContainerId = containerId;
     }
 
     /**
      * Create an instance for a specific container.
      */
-    public static FragmentStack create(FragmentActivity activity, int containerId) {
-        return new FragmentStack(activity, containerId);
+    public static FragmentStack create(FragmentManager fragmentManager, int containerId) {
+        return new FragmentStack(fragmentManager, containerId);
     }
 
     public int size() {
@@ -41,85 +39,112 @@ public final class FragmentStack {
      * Replaces the entire mStack with this fragment.
      */
     public void replace(Fragment fragment, String tag) {
-        if (fragment != null) {
-            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        if (fragment == null) {
+            return;
+        }
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        if (fragment.isAdded()) {
+            clear(transaction, fragment);
+            showFragment(transaction, fragment, tag);
+        } else {
             clear(transaction);
             push(transaction, fragment, tag);
-            transaction.commit();
         }
+        transaction.commit();
     }
 
-    /**
-     * replace top fragment only
-     */
-    public void replaceTop(Fragment fragment, String tag) {
-        if (fragment != null) {
-            FragmentTransaction transaction = mFragmentManager.beginTransaction();
-            Fragment topFragment = mStack.pollLast();
-            removeFragment(transaction, topFragment);
-            showFragment(transaction, fragment, tag);
-            transaction.commit();
-        }
+    public LinkedList<Fragment> getLinkedList() {
+        return mStack;
     }
-
 
     /**
      * Adds a new fragment to the mStack and displays it.
      */
     public void push(Fragment fragment, String tag) {
-        if (fragment != null) {
-            FragmentTransaction transaction = mFragmentManager.beginTransaction();
-            showFragment(transaction, fragment, tag);
-            mStack.add(fragment);
+        if (fragment == null) {
+            return;
         }
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        if (getLinkedList().size() >= 1) {
+            transaction
+                    .setCustomAnimations(R.anim.activity_right_in, R.anim.activity_left_out, 0, 0);
+        }
+        showFragment(transaction, fragment, tag);
+        transaction.commit();
     }
 
-    private void push(FragmentTransaction transaction, Fragment fragment, String tag) {
-        if (fragment != null && !fragment.isAdded()) {
-            detachTop(transaction);
-            transaction.add(fragment, tag);
-            mStack.add(fragment);
+    public Fragment pop() {
+        if (mStack.size() > 1) {
+            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+            transaction
+                    .setCustomAnimations(R.anim.activity_left_in, R.anim.activity_right_out, 0, 0);
+            Fragment popFragment = mStack.pollLast();
+            removeFragment(transaction, popFragment);
+            Fragment f = mStack.peekLast();
+            showFragment(transaction, f, f.getTag());
+            transaction.commit();
+            return popFragment;
+        }
+        return null;
+    }
+
+    public boolean hasBackStack() {
+        return mStack.size() > 1;
+    }
+
+    public void removeFragment(Fragment fragment) {
+        if (fragment != null && fragment.isAdded()) {
+            mFragmentManager.beginTransaction().remove(fragment).commit();
+            if (mStack.contains(fragment)) {
+                mStack.remove(fragment);
+            }
         }
     }
 
     private void detachTop(FragmentTransaction transaction) {
         if (mStack.size() > 0) {
             Fragment f = mStack.peekLast();
-            hideFragment(transaction, f);
-        }
-    }
-
-    /**
-     * Removes the fragment at the top of the mStack and displays the previous one. This will not do
-     * anything if there is
-     * only one fragment in the mStack.
-     *
-     * @return Whether a transaction has been enqueued.
-     */
-    public void pop() {
-        if (mStack.size() > 1) {
-            FragmentTransaction transaction = mFragmentManager.beginTransaction();
-            removeFragment(transaction, mStack.pollLast());
-            Fragment f = mStack.peekLast();
-            showFragment(transaction, f, f.getTag());
-            transaction.commit();
+            if (f != null && f.isAdded() && !f.isHidden()) {
+                hideFragment(transaction, f);
+            }
         }
     }
 
     private void showFragment(FragmentTransaction transaction, Fragment fragment, String tag) {
+        if (fragment == null) {
+            return;
+        }
         if (fragment.isAdded()) {
             if (fragment.isHidden()) {
+                detachTop(transaction);
                 transaction.show(fragment);
+                mStack.remove(fragment);
+                mStack.addLast(fragment);
+                if (mTopFragmentChangeListener != null) {
+                    mTopFragmentChangeListener.onTopFragmentChange(fragment);
+                }
             }
+            // fragment is in show state, do nothing.
         } else {
+            //fragment is not added, add and show fragment.
             push(transaction, fragment, tag);
         }
     }
 
+    /**
+     * hide fragment divide hide() and remove()
+     *
+     * @param transaction
+     * @param fragment
+     */
     private void hideFragment(FragmentTransaction transaction, Fragment fragment) {
-        if (fragment.isAdded() && !fragment.isHidden()) {
-            transaction.hide(fragment);
-        }
+        transaction.hide(fragment);
+    }
+
+    public void clear() {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        clear(transaction);
+        transaction.commit();
     }
 
     private void clear(FragmentTransaction transaction) {
@@ -129,9 +154,51 @@ public final class FragmentStack {
         }
     }
 
+    /**
+     * clear stack but except one fragment which is already in stack.
+     *
+     * @param transaction
+     * @param exceptFragment fragment is in stack
+     */
+    private void clear(FragmentTransaction transaction, Fragment exceptFragment) {
+        while (mStack.size() > 0) {
+            Fragment fragment = mStack.pollLast();
+            if (fragment != exceptFragment) {
+                removeFragment(transaction, fragment);
+            }
+        }
+        mStack.addLast(exceptFragment);
+    }
+
     private void removeFragment(FragmentTransaction transaction, Fragment fragment) {
-        if (fragment != null && fragment.isAdded()) {
+        if (fragment == null) {
+            return;
+        }
+        if (fragment.isAdded()) {
             transaction.remove(fragment);
         }
+    }
+
+    public void setTopFragmentChangeListener(TopFragmentChangeListener topFragmentChangeListener) {
+        mTopFragmentChangeListener = topFragmentChangeListener;
+    }
+
+    public interface TopFragmentChangeListener {
+        void onTopFragmentChange(Fragment topFragment);
+    }
+
+    private void push(FragmentTransaction transaction, Fragment fragment, String tag) {
+        if (fragment != null && !fragment.isAdded()) {
+            detachTop(transaction);
+            transaction.add(mContainerId, fragment, tag);
+            if (mTopFragmentChangeListener != null) {
+                mTopFragmentChangeListener.onTopFragmentChange(fragment);
+            }
+            mStack.addLast(fragment);
+        }
+    }
+
+    public Fragment getTopShowFragment() {
+        return mStack.peekLast();
     }
 }
--- app/src/main/java/com/smart/control/section/MainActivity.java
+++ app/src/main/java/com/smart/control/section/MainActivity.java
@@ -1,13 +1,55 @@
 package com.smart.control.section;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FragmentStack mFragmentStack;
+    public static final String KEY_COUNT = "count";
+    private int mCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragmentStack = FragmentStack.create(getSupportFragmentManager(), R.id.fl_container);
+        findViewById(R.id.btn_push).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCount++;
+                TextFragment fragment = new TextFragment();
+                Bundle args = new Bundle();
+                args.putInt(KEY_COUNT, mCount);
+                fragment.setArguments(args);
+                mFragmentStack.push(fragment, null);
+            }
+        });
+
+        findViewById(R.id.btn_pop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFragmentStack.hasBackStack()) {
+                    mCount--;
+                    mFragmentStack.pop();
+                }
+            }
+        });
+
+        findViewById(R.id.btn_replace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFragmentStack.replace(new TextFragment3(), null);
+            }
+        });
+
+        findViewById(R.id.btn_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFragmentStack.clear();
+            }
+        });
     }
 }
